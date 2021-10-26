97c82ffa1defcfc96cf297dc2cb757d56d5968fb
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-26 20:21:24 +0200

    liber: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: motorola Model: motorola one fusion+ Script version: 2.0.1 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_liber.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_liber-user \
+    omni_liber-userdebug \
+    omni_liber-eng
